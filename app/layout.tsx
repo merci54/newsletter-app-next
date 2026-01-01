07b116ae077d78d07d3a2c858f943cd7b4d25649
Yaroslav <yaroslavlit@gmail.com>
2026-01-01 15:33:32 +0100

    add main landmark

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body
         className={roboto.variable}
       >
-        {children}
+        <main>{children}</main>
       </body>
     </html>
   );
